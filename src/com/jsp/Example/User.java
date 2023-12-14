146ef6430ba01787f0bd06a5125502413fc97e56
chandantp1407 <chandantp1407@gmail.com>
2023-12-14 00:18:22 -0800

    user data created

---
+++ src/com/jsp/Example/User.java
@@ -0,0 +1,34 @@
+package com.jsp.Example;
+
+public class User {
+	private int userId;
+	private String userName;
+	private String UserEmail;
+	private String password;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserEmail() {
+		return UserEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		UserEmail = userEmail;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+}
